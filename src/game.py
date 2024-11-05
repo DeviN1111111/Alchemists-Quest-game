7be1f8074b18cf5b53c96993f878f48dd83cc941
EyeMustache <lucioridder2003@gmail.com>
2024-11-05 13:01:55 +0100

    trying tiled

--- src/game.py
+++ src/game.py
@@ -1,6 +1,60 @@
-import pygame
-import math
+import pygame, math, csv, os
 from settings import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, spritesheet):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = spritesheet.parse_sprite(image)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class TileMap:
+    def __init__(self, filename, spritesheet):
+        self.tile_size = 32
+        self.start_x, self.start_y = 0, 0
+        self.spritesheet = spritesheet
+        self.tiles = self.load_tiles(filename)
+        self.map_surface = pygame.Surface((self.map_w, self.map_h))
+        self.map_surface.set_colorkey((0, 0, 0))
+        self.load_map()
+
+    def load_map(self):
+        for tile in self.tiles:
+            tile.draw(self.map_surface)
+
+    def draw_map(self, surface):
+        surface.blit(self.map_surface, (0, 0))
+
+    def read_csv(self, filename):
+        map = []
+        with open(os.path.join(filename)) as data:
+            data = csv.reader(data, delimiter=",")
+            for row in data:
+                map.append(list(row))
+        return map
+
+    def load_tiles(self, filename):
+        tiles = []
+        map = self.read_csv(filename)
+
+        x, y = 0, 0
+        for row in map:
+            x = 0
+            for tile in row:
+                if tile == "11":
+                    tiles.append(Tile("grass.png", x * self.tile_size, y * self.tile_size, self.spritesheet))
+                elif tile == "51":
+                    tiles.append(Tile("dirt.png", x * self.tile_size, y * self.tile_size, self.spritesheet))
+                x += 1
+            y += 1
+
+        self.map_w, self.map_h = x * self.tile_size, y * self.tile_size
+        return tiles
 
 
 class Player:
@@ -19,7 +73,7 @@ class Player:
     def movement(self, keys):
         dx = 0
         dy = 0
-    
+
         if keys[pygame.K_a]:
             dx -= 1
         if keys[pygame.K_d]:
@@ -31,7 +85,7 @@ class Player:
 
         # Normalize movement vector for consistent speed
         if dx != 0 or dy != 0:
-            length = math.sqrt(dx ** 2 + dy ** 2)
+            length = math.sqrt(dx**2 + dy**2)
             dx /= length  # Normalize the x component
             dy /= length  # Normalize the y component
             self.x += dx * self.speed
